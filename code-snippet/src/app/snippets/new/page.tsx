37502730c19c1d750c55907a7e6c6cc8e2835f9b
bipinkatuwal567 <bipinkatuwal098@gmail.com>
2024-03-25 23:59:03 +0545

    added: creating snippet feature and connected with db and fetched data

---
+++ code-snippet/src/app/snippets/new/page.tsx
@@ -0,0 +1,53 @@
+import { db } from "@/db";
+import { redirect } from "next/navigation";
+
+export default function SnippetCreatePage() {
+    async function createSnippet(formData: FormData){
+        // This need to be a server action!
+        'use server';
+
+        // Check the user's inputs and make sure they're valid
+        const title = formData.get("title") as string;
+        const code = formData.get("code") as string;
+
+        // Create a new record in the database
+        const snippet = await db.snippet.create({
+            data: {
+                title,
+                code
+            }
+        });
+        console.log("Snippet: ",snippet);
+
+        // Redirect the user back to ther root route
+        redirect("/");
+    }
+    
+  return (
+    <form action={createSnippet}>
+      <h3 className="font-semibold text-lg my-3">Create a snippet!</h3>
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="title">Title</label>
+          <input
+            type="text"
+            name="title"
+            id="title"
+            className="border w-full rounded p-2"
+          />
+        </div>
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="code">Code</label>
+          <textarea
+            name="code"
+            id="code"
+            className="border w-full rounded p-2"
+          />
+        </div>
+        <button type="submit" className="rounded p-2 bg-blue-200">
+          Create
+        </button>
+      </div>
+    </form>
+  );
+}
