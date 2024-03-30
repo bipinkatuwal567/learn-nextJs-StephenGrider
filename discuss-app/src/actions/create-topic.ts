b272709cd4c21f1b0a5f0ebbbe4705c5a2b11e34
bipinkatuwal567 <bipinkatuwal098@gmail.com>
2024-03-30 15:38:09 +0545

    added: zod library to validate a create topic form and completed that component

--- discuss-app/src/actions/create-topic.ts
+++ discuss-app/src/actions/create-topic.ts
@@ -1,8 +1,79 @@
 "use server";
 
-export async function createTopic(formData: FormData){
-    const name = formData.get("name");
-    const description = formData.get("description");
+import { auth } from "@/auth";
+import { db } from "@/db/script";
+import path from "@/helper";
+import { Topic } from "@prisma/client";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { z } from "zod";
 
-    console.log(name, description);
+const CreateTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/[a-b-]/, {
+      message: "Name must be lowercase letter with dash and no any space",
+    }),
+  description: z.string().min(10),
+});
+
+interface CreateTopicFormState {
+  error: {
+    name?: string[];
+    description?: string[];
+    _form?: string[];
+  };
+}
+
+export async function createTopic(
+  formState: CreateTopicFormState,
+  formData: FormData
+): Promise<CreateTopicFormState> {
+  const result = CreateTopicSchema.safeParse({
+    name: formData.get("name"),
+    description: formData.get("description"),
+  });
+
+  if (!result.success) {
+    return {
+      error: result.error.flatten().fieldErrors,
+    };
+  }
+
+  const session = await auth();
+  if (!session || !session.user) {
+    return {
+      error: {
+        _form: ["You must be signed in to create a form!"],
+      },
+    };
+  }
+
+  let topic: Topic;
+  try {
+    topic = await db.topic.create({
+      data: {
+        slug: result.data.name,
+        description: result.data.description,
+      },
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        error: {
+          _form: [err.message],
+        },
+      };
+    } else {
+      return {
+        error: {
+          _form: ["Something went wrong!"],
+        },
+      };
+    }
+  }
+
+  revalidatePath("/");
+  redirect(path.topicShowPath(topic.slug));
 }
--- discuss-app/src/components/topics/topic-create-form.tsx
+++ discuss-app/src/components/topics/topic-create-form.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import * as actions from "@/actions/index";
 import {
   Button,
@@ -7,15 +9,22 @@ import {
   PopoverTrigger,
   Textarea,
 } from "@nextui-org/react";
+import { useFormState } from "react-dom";
 
 export default function TopicCreateForm() {
+  const [formState, action] = useFormState(actions.createTopic, {
+    error: {},
+  });
+
   return (
     <Popover>
       <PopoverTrigger>
-        <Button color="primary" variant="shadow">Create a topic</Button>
+        <Button color="primary" variant="shadow">
+          Create a topic
+        </Button>
       </PopoverTrigger>
       <PopoverContent>
-        <form action={actions.createTopic}>
+        <form action={action}>
           <div className="flex flex-col gap-4 p-4 w-80">
             <h2 className="text-base font-medium">Create a topic</h2>
             <Input
@@ -23,13 +32,22 @@ export default function TopicCreateForm() {
               label="Name"
               labelPlacement="outside"
               placeholder="Name"
+              isInvalid={!!formState.error.name}
+              errorMessage={formState.error.name?.join(", ")}
             />
             <Textarea
               name="description"
               label="Description"
               labelPlacement="outside"
               placeholder="Describe your  topic"
+              isInvalid={!!formState.error.description}
+              errorMessage={formState.error.description?.join(", ")}
             />
+            {formState.error._form && (
+              <div className="text-xs text-red-500">
+                {formState.error._form.join(", ")}
+              </div>
+            )}
             <Button type="submit">Submit</Button>
           </div>
         </form>
